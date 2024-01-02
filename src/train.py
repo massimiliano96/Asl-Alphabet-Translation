766e65e576d02ee3a92c1dbcc738a55144203340
massimiliano96 <massimilianoriva96@gmail.com>
2024-01-02 12:00:26 +0100

    Modified Ultralytics MLflow integration to prevent premature run termination at the conclusion of training.

--- src/train.py
+++ src/train.py
@@ -3,11 +3,86 @@ import shutil
 
 import mlflow
 import yaml
-
-# from dagshub import dagshub_logger
 from ultralytics import YOLO, settings
+from ultralytics.utils import LOGGER, RUNS_DIR, colorstr
 
-settings.update({"mlflow": True})
+PREFIX = colorstr("MLflow: ")
+
+
+def sanitize_values(x):
+    return {k.replace("(", "").replace(")", ""): float(v) for k, v in x.items()}
+
+
+SANITIZE = sanitize_values
+
+
+settings.update({"mlflow": False})
+
+"""
+These methods are crucial for customizing the behavior of Ultralytics YOLOv8 MLflow integration.
+The default on_train_end callback, by default, concludes the MLflow run.
+However, to include the test metrics in the same run,
+I have made modifications to this callback to address this specific requirement.
+"""
+
+
+def on_pretrain_routine_end(trainer):
+    """Log training parameters to MLflow at the end of the pretraining routine."""
+    global mlflow
+
+    uri = os.environ.get("MLFLOW_TRACKING_URI") or str(RUNS_DIR / "mlflow")
+    LOGGER.debug(f"{PREFIX} tracking uri: {uri}")
+    mlflow.set_tracking_uri(uri)
+
+    # Set experiment and run names
+    experiment_name = (
+        os.environ.get("MLFLOW_EXPERIMENT_NAME")
+        or trainer.args.project
+        or "/Shared/YOLOv8"
+    )
+    run_name = os.environ.get("MLFLOW_RUN") or trainer.args.name
+    mlflow.set_experiment(experiment_name)
+
+    mlflow.autolog()
+    try:
+        active_run = mlflow.active_run() or mlflow.start_run(run_name=run_name)
+        LOGGER.info(f"{PREFIX}logging run_id({active_run.info.run_id}) to {uri}")
+        mlflow.log_params(dict(trainer.args))
+    except Exception as e:
+        LOGGER.warning(
+            f"{PREFIX}WARNING ⚠️ Failed to initialize: {e}\n"
+            f"{PREFIX}WARNING ⚠️ Not tracking this run"
+        )
+
+
+def on_train_epoch_end(trainer):
+    """Log training metrics at the end of each train epoch to MLflow."""
+    if mlflow:
+        mlflow.log_metrics(
+            metrics=SANITIZE(trainer.label_loss_items(trainer.tloss, prefix="train")),
+            step=trainer.epoch,
+        )
+        mlflow.log_metrics(metrics=SANITIZE(trainer.lr), step=trainer.epoch)
+
+
+def on_fit_epoch_end(trainer):
+    """Log training metrics at the end of each fit epoch to MLflow."""
+    if mlflow:
+        mlflow.log_metrics(metrics=SANITIZE(trainer.metrics), step=trainer.epoch)
+
+
+def on_train_end(trainer):
+    """Log model artifacts at the end of the training."""
+    if mlflow:
+        mlflow.log_artifact(
+            str(trainer.best.parent)
+        )  # log save_dir/weights directory with best.pt and last.pt
+        for f in trainer.save_dir.glob("*"):  # log all other files in save_dir
+            if f.suffix in {".png", ".jpg", ".csv", ".pt", ".yaml"}:
+                mlflow.log_artifact(str(f))
+
+        LOGGER.info(f"{PREFIX}results logged to {mlflow.get_tracking_uri()}\n")
+
 
 # Read the run ID from the file
 with open("run_id.txt", "r") as file:
@@ -44,6 +119,11 @@ os.mkdir(directory_output)
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
+model.add_callback("on_train_end", on_train_end)
+model.add_callback("on_pretrain_routine_end", on_pretrain_routine_end)
+model.add_callback("on_train_epoch_end", on_train_epoch_end)
+model.add_callback("on_fit_epoch_end", on_fit_epoch_end)
+
 # Use the model
 results = model.train(
     data="yolo-config.yaml",
@@ -64,5 +144,3 @@ mlflow.log_param("batch", batch_size)
 mlflow.log_param("lr0", init_lr)
 mlflow.log_param("patience", patience)
 mlflow.log_param("optimizer", optimizer)
-
-mlflow.log_artifact("output/train/confusion_matrix_normalized.png")
